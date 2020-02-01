c033080fe1b2484b8b9656b7e1c4ac85504faa1d
Squinky <hey@squinky.me>
2020-02-01 13:03:24 -0500

    test audio

--- clickable.gd
+++ clickable.gd
@@ -7,4 +7,6 @@ export (String) var id
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
+			var soundarray = $sounds.get_children()
+			soundarray[randi() % soundarray.size()].play()
 			emit_signal("clicked", id)
