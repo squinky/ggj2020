b0461a5b0ba86630249b8f43c915dca524864631
Squinky <hey@squinky.me>
2020-02-02 13:47:03 -0500

    added a bunch of sound and text

--- text.gd
+++ text.gd
@@ -3,13 +3,42 @@ extends Node
 var dictionary = {
 	"start": "Recovering from surgery should be easy, right?",
 	"ouch": "OW OW OW OW OW OW OW OW OW OW OW OW OW",
+	"cat": [
+		"I’d scratch that belly SO HARD if I could only reach it.",
+		"Me-owwwww.",
+		"Are you trying to comfort me or do I just make an excellent pillow?",
+		"Purr purr purr, you said it.",
+		"Watcha gonna do with all that fur, all that fur that’s up on her?",
+		"I’ma purr purr purr purr purr purr, purr up there with all that fur."
+	],
 	"burrito": [
 		"SO BORED",
 		"When does roommate come home?",
 		"I hurt all over.",
 		"What time is it?",
 		"It is a human burrito!",
-		"Maybe more goblin than human - haven't seen the sun in a while."
+		"Maybe more goblin than human - haven't seen the sun in a while.",
+		"I forget what faces look like."
+	],
+	"plant": [
+		"Looks thirsty, but I can’t water it from here.",
+		"Is it still alive?",
+		"O sun-drenched beauty! O wondrous drinker!",
+		"Rustle your leaves to me softly.",
+		"You’re still my friend, right?"
+	],
+	"water": [
+		"It’s very far to reach.",
+		"So thirsty. But it’s too far away.",
+		"Half empty, half full, or just too much glass in the first place?",
+		"Better not try to reach for it...",
+		"A glass of water is basically a domesticated puddle. Think about it."
+	],
+	"pills": [
+		"Is it that time yet?",
+		"Sweet, sweet drugs.",
+		"What does “rx” even mean? Is it a type of dinosaur?",
+		"Why can’t painkillers come in fun colours and shapes, like chewable vitamins?"
 	],
 	"door": [
 		"Oh no, no way to be ready for that yet.",
@@ -17,10 +46,9 @@ var dictionary = {
 		"A surfboard with hinges, beyond which lies a terrifying ocean.",
 		"What lies beyond?"
 	],
-	"pills": [
-		"Is it that time yet?",
-		"Sweet, sweet drugs.",
-		"What does “rx” even mean? Is it a type of dinosaur?"
+	"sandwich": [
+		"That sandwich just gets less and less appetizing the longer it sits there.",
+		"I'm really not hungry."
 	],
 	"tv": [
 		"*tv noises*"
