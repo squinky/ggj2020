0c7c7a3c69e0826253fa082283a368fb2cd7d55b
Squinky <hey@squinky.me>
2020-02-02 16:49:20 -0500

    more text yay

--- text.gd
+++ text.gd
@@ -2,19 +2,19 @@ extends Node
 
 var dictionary = {
 	"start": [
-		"All I gotta do is lie here. Sounds easy enough, right?"
+		"Okay, so all I gotta do is lie here and do nothing for a good long time. Sounds easy enough, right?"
 	],
 	"ouch": [
 		"OW OW OW OW OW OW OW OW OW OW OW OW OW",
-		"OW OW OW TOO SOON OW OW OW"
+		"OW OW OW TOO SOON OW OW OW",
+		"OW OW OW WHY DID I DO THAT OW OW OW"
 	],
 	"cat": [
 		"I’d scratch that belly SO HARD if I could only reach it.",
 		"Me-owwwww.",
 		"Are you trying to comfort me or do I just make an excellent pillow?",
 		"Purr purr purr, you said it.",
-		"Watcha gonna do with all that fur, all that fur that’s up on her?",
-		"I’ma purr purr purr purr purr purr, purr up there with all that fur.",
+		"Watcha gonna do with all that fur, all that fur that’s up on her? I’ma purr purr purr purr purr purr, purr up there with all that fur.",
 		"Now here's a cat who knows where its pyjamas are.",
 		"This is the sweetest cat. I think it's guarding me from danger.",
 		"Probably just stealing my heat, but still pretty sweet."
@@ -28,7 +28,7 @@ var dictionary = {
 		"It doesn't hurt so long as I don't move any part of my body!",
 		"Feeling fine, beyond the stitches holding it all together.",
 		"Maybe more goblin than human - haven't seen the sun in a while.",
-		"What a fine-looking swamp creature -- great hair!",
+		"What a fine-looking swamp creature - great hair!",
 		"A non-binary babe if I ever saw one!",
 		"A cocoon of soft. - The burrito snores loudly. - Ouch! Time heals all wounds.",
 		"I forget what faces look like."
@@ -55,20 +55,20 @@ var dictionary = {
 	"pills": [
 		"Is it that time yet?",
 		"Sweet, sweet drugs.",
-		"What does “rx” even mean? Is it a type of dinosaur?",
+		"What does 'rx' even mean? Is it a type of dinosaur?",
 		"Wouldn't want to overdo it with those.",
 		"Why do we call them pills anyway? Is it a portmanteau of 'potion' and 'ill'? ... Probably not, but it should be.",
 		"Healing takes time. These help a little.",
-		"Why can’t painkillers come in fun colours and shapes, like chewable vitamins?"
+		"Why can’t prescription-grade painkillers come in fun colours and shapes, like chewable vitamins? Oh, right."
 	],
 	"door": [
 		"Oh no, no way to be ready for that yet.",
-		"The door itself is too far away, let alone the world on the other side of it.",
+		"The door itself is too far away, let alone the terrifyingly massive world on the other side of it.",
 		"A surfboard with hinges, beyond which lies a terrifying ocean.",
 		"What lies beyond?",
 		"Outside: the formless void, a barbaric yawp of horror and pain.",
 		"Yeah, probably shouldn't try to go out yet.",
-		"Should I stay or should I go now? If I go there will be trouble, if I stay there will be...boredom.",
+		"Should I stay or should I go now? If I go there will be trouble, if I stay there will be... boredom.",
 		"What's so great about sunlight and socializing with other humans anyway?",
 		"Can't wait for sweet, sweet freedom. Won't even complain about the weather!"
 	],
@@ -77,19 +77,20 @@ var dictionary = {
 		"I'm really not hungry.",
 		"Sustenance. But that bread looks awful dry.",
 		"If I stare at it long enough, maybe I can pretend it's sushi.",
-		"A half-eaten sandwich. Yum."
+		"A half-eaten sandwich. Yum.",
+		"Not even the cat is interested in this sandwich, which says it all, really."
 	],
 	"tv": [
-		"*tv noises*",
 		"After a while, it all kind of blurs together.",
 		"Wow, 90s children movies are actually really racist.",
 		"Daytime television is its own special kind of hell.",
 		"Why is choosing what to watch so hard?",
-		"You'd think having permission to just watch TV all day would be awesome."
+		"You'd think having permission to just watch TV all day would be awesome.",
+		"I wish I had some lo-fi beats to chill to instead of whatever this is."
 	],
 	"phone": [
 		"Not even twitter memes will save me.",
-		"My phone -- a portal to the outside world and my friends.",
+		"My phone - a portal to the outside world and my friends.",
 		"Let's look up the queer monarchs of England on Wikipedia... Wow, King James gave them a whole Bible to leave him alone.",
 		"Actually...let's not check the news right now.",
 		"I don't want to be a burden to my friends... I know I'm not, but it still feels that way sometimes.",
