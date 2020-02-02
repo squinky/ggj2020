e9758a739eee8bc25ac4df48bb3464985c2d88f0
Jess Marcotte <meaikoh@gmail.com>
2020-02-02 16:03:51 -0500

    some added quips

--- text.gd
+++ text.gd
@@ -9,7 +9,10 @@ var dictionary = {
 		"Are you trying to comfort me or do I just make an excellent pillow?",
 		"Purr purr purr, you said it.",
 		"Watcha gonna do with all that fur, all that fur that’s up on her?",
-		"I’ma purr purr purr purr purr purr, purr up there with all that fur."
+		"I’ma purr purr purr purr purr purr, purr up there with all that fur.",
+		"Now here's a cat who knows where its pyjamas are.",
+		"This is the sweetest cat. I think it's guarding me from danger.",
+		"Probably just stealing my heat, but still pretty sweet."
 	],
 	"burrito": [
 		"SO BORED",
@@ -17,7 +20,12 @@ var dictionary = {
 		"I hurt all over.",
 		"What time is it?",
 		"It is a human burrito!",
+		"It doesn't hurt so long as I don't move any part of my body!",
+		"Feeling fine, beyond the stitches holding it all together.",
 		"Maybe more goblin than human - haven't seen the sun in a while.",
+		"What a fine-looking swamp creature -- great hair!",
+		"A non-binary babe if I ever saw one!",
+		"A cocoon of soft. - The burrito snores loudly. - Ouch! Time heals all wounds.",
 		"I forget what faces look like."
 	],
 	"plant": [
@@ -25,6 +33,9 @@ var dictionary = {
 		"Is it still alive?",
 		"O sun-drenched beauty! O wondrous drinker!",
 		"Rustle your leaves to me softly.",
+		"Ever wonder what your plants are thinking?",
+		"Do plants have goals - desires beyond just surviving and propagating?",
+		"I once accidentally mummified a plant.",
 		"You’re still my friend, right?"
 	],
 	"water": [
@@ -32,23 +43,36 @@ var dictionary = {
 		"So thirsty. But it’s too far away.",
 		"Half empty, half full, or just too much glass in the first place?",
 		"Better not try to reach for it...",
+		"Got to stay hydrated, but that glass is out of reach.",
+		"Time to work on my telekinesis -- !!!!! -- Nope.",
 		"A glass of water is basically a domesticated puddle. Think about it."
 	],
 	"pills": [
 		"Is it that time yet?",
 		"Sweet, sweet drugs.",
 		"What does “rx” even mean? Is it a type of dinosaur?",
+		"Wouldn't want to overdo it with those.",
+		"Why do we call them pills anyway? Is it a portmanteau of 'potion' and 'ill'? ... Probably not.",
+		"Healing takes time. These help a little.",
 		"Why can’t painkillers come in fun colours and shapes, like chewable vitamins?"
 	],
 	"door": [
 		"Oh no, no way to be ready for that yet.",
 		"The door itself is too far away, let alone the world on the other side of it.",
 		"A surfboard with hinges, beyond which lies a terrifying ocean.",
-		"What lies beyond?"
+		"What lies beyond?",
+		"Outside: the formless void, a barbaric yawp of horror and pain.",
+		"Yeah, probably shouldn't try to go out yet.",
+		"Should I stay or should I go now? If I go there will be trouble, if I stay there will be...boredom.",
+		"What's so great about sunlight and socializing with other humans anyway?",
+		"Can't wait for sweet, sweet freedom. Won't even complain about the weather!"
 	],
 	"sandwich": [
 		"That sandwich just gets less and less appetizing the longer it sits there.",
-		"I'm really not hungry."
+		"I'm really not hungry.",
+		"Sustenance. But that bread looks awful dry.",
+		"If I stare at it long enough, maybe I can pretend it's sushi.",
+		"A half-eaten sandwich. Yum."
 	],
 	"tv": [
 		"*tv noises*"
